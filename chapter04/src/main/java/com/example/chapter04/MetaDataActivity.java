724f0f4195753630fa7b1bf8656cd987e4e41cd2
skychx <skychx@hotmail.com>
2022-02-02 18:38:06 +0800

    4.Activity

--- chapter04/src/main/java/com/example/chapter04/MetaDataActivity.java
+++ chapter04/src/main/java/com/example/chapter04/MetaDataActivity.java
@@ -23,10 +23,12 @@ public class MetaDataActivity extends AppCompatActivity {
     // 显示配置的元数据
     private void showMetaData() {
         try {
-            PackageManager pm = getPackageManager(); // 获取应用包管理器
-            // 从应用包管理器中获取当前的活动信息
+            // 1。获取应用包管理器
+            PackageManager pm = getPackageManager();
+            // 2。从应用包管理器中获取当前的 Activity 信息
             ActivityInfo act = pm.getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
-            Bundle bundle = act.metaData; // 获取活动附加的元数据信息
+            // 3。获取活动附加的元数据信息
+            Bundle bundle = act.metaData;
             String value = bundle.getString("weather"); // 从包裹中取出名叫weather的字符串
             tv_meta.setText("来自元数据信息：今天的天气是"+value); // 在文本视图上显示文字
         } catch (Exception e) {
